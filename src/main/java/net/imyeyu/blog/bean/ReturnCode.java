c61f225380f8b22cab7615430b64bbb1b5b43fef
Nagiasu <imyeyu@qq.com>
2021-05-24 15:35:30 +0800

    名词在前，形容在后

--- src/main/java/net/imyeyu/blog/bean/ReturnCode.java
+++ src/main/java/net/imyeyu/blog/bean/ReturnCode.java
@@ -10,13 +10,15 @@ import lombok.Getter;
 @Getter
 public enum ReturnCode {
 
-	SUCCESS         (20000, "执行成功"),
-	MISS_PARAMS     (40000, "缺少参数"),
-	BAD_PARAMS      (40001, "不合法的参数"),
-	MISS_PERMISSION (40100, "无权限"),
-	ERROR_PERMISSION(40101, "无权限"),
-	NULL_RESULT     (40400, "无结果"),
-	ERROR           (50000, "服务端异常");
+	SUCCESS          (20000, "执行成功"),
+	PARAMS_MISS      (40000, "缺少参数"),
+	PARAMS_BAD       (40001, "不合法的参数"),
+	PARAMS_EXPIRD    (40002, "过期的参数"),
+	PERMISSION_MISS  (40100, "无权限"),
+	PERMISSION_ERROR (40101, "无权限"),
+	REQUEST_BAD      (40102, "非法请求"),
+	RESULT_NULL      (40400, "无结果"),
+	ERROR            (50000, "服务端异常");
 
 	private final Integer code;
 	private final String comment;

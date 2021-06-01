5cf9bbf9a440c388a581cd2ecb0208d67c3f6e91
Nagiasu <imyeyu@qq.com>
2021-06-01 09:14:02 +0800

    序列化基本实体

--- src/main/java/net/imyeyu/blog/entity/BaseEntity.java
+++ src/main/java/net/imyeyu/blog/entity/BaseEntity.java
@@ -2,13 +2,15 @@ package net.imyeyu.blog.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 基本实体
  *
  * 夜雨 创建于 2021-05-20 11:38
  */
 @Data
-public class BaseEntity {
+public class BaseEntity implements Serializable {
 
 	protected Long id;
 	protected Long createdAt;

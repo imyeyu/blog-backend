f9bf8f79fde7237db6a1d9186e91a03484cc959d
Nagiasu <imyeyu@qq.com>
2021-07-01 21:46:27 +0800

    更新 BetterJava

--- src/main/java/net/imyeyu/blog/bean/Response.java
+++ src/main/java/net/imyeyu/blog/bean/Response.java
@@ -1,8 +1,8 @@
 package net.imyeyu.blog.bean;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
+import net.imyeyu.betterjava.bean.BlogResponse;
 
 /**
  * 返回前端数据对象
@@ -10,27 +10,22 @@ import lombok.NoArgsConstructor;
  * 夜雨 创建于 2021-05-20 00:08
  */
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Response<T> {
-
-	private Integer code;
-	private String msg;
-	private T data;
+@EqualsAndHashCode(callSuper = true)
+public class Response<T> extends BlogResponse<T> {
 
 	public Response(ReturnCode code) {
-		this(code.getCode(), "", null);
+		super(code.getCode(), "", null);
 	}
 
 	public Response(ReturnCode code, Throwable e) {
-		this(code.getCode(), e.getMessage(), null);
+		super(code.getCode(), e.getMessage(), null);
 	}
 
 	public Response(ReturnCode code, String msg) {
-		this(code.getCode(), msg, null);
+		super(code.getCode(), msg, null);
 	}
 
 	public Response(ReturnCode code, T data) {
-		this(code.getCode(), "", data);
+		super(code.getCode(), "", data);
 	}
 }

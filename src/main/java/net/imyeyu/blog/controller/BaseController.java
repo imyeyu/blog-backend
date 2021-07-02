018478273729b013e178429c5a9ea2c488a25384
Nagiasu <imyeyu@qq.com>
2021-07-02 14:14:25 +0800

    GSON 静态于基本控制器

--- src/main/java/net/imyeyu/blog/controller/BaseController.java
+++ src/main/java/net/imyeyu/blog/controller/BaseController.java
@@ -1,9 +1,13 @@
 package net.imyeyu.blog.controller;
 
+import com.google.gson.Gson;
+
 /**
  * 基本控制器
  *
  * 夜雨 创建于 2021/2/23 21:37
  */
 public class BaseController {
+
+	protected static final Gson GSON = new Gson();
 }

5071e22c1b4ab1f50bb2b67c8684aa12687d9468
Nagiasu <imyeyu@qq.com>
2021-07-05 09:15:40 +0800

    独立文章类型、分类和标签结构

--- src/main/java/net/imyeyu/blog/entity/Article.java
+++ src/main/java/net/imyeyu/blog/entity/Article.java
@@ -1,9 +1,11 @@
 package net.imyeyu.blog.entity;
 
+import com.google.gson.JsonObject;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * 文章
@@ -15,17 +17,17 @@ import java.io.Serializable;
 public class Article extends BaseEntity implements Serializable {
 
 	private String title;
-	private String type;
-	private String clazz;
-	private String label;
+	private ArticleType type;
+	private ArticleClass clazz;
+	private List<ArticleLabel> labels;
 	private String digest;
 	private String data;
+	private JsonObject extendData;
 
 	private int reads;
 	private int likes;
 	private int comments;
-
-	private boolean isHide;
+	private boolean canComment;
 
 	/** 评论一次 */
 	public void comment() {

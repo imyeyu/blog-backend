620270177fce56f1a011b31da3d237c8752da0d2
Nagiasu <imyeyu@qq.com>
2021-07-05 09:08:41 +0800

    文章标签服务

---
+++ src/main/java/net/imyeyu/blog/service/ArticleLabelService.java
@@ -0,0 +1,13 @@
+package net.imyeyu.blog.service;
+
+import net.imyeyu.blog.entity.ArticleLabel;
+
+/**
+ * 文章标签服务
+ *
+ * 夜雨 创建于 2021-07-04 19:33
+ */
+public interface ArticleLabelService extends BaseService<ArticleLabel>{
+
+	ArticleLabel findByName(String name);
+}

f6a515650ed22a4d65dcf8ec4ff13dfb6e29d7b9
Nagiasu <imyeyu@qq.com>
2021-07-16 10:41:01 +0800

    携带令牌的请求体

---
+++ src/main/java/net/imyeyu/blogapi/bean/TokenData.java
@@ -0,0 +1,19 @@
+package net.imyeyu.blogapi.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <p>携带登录令牌的请求数据
+ *
+ * <p>夜雨 创建于 2021-07-14 10:46
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenData<T> {
+
+	private String token;
+	private T data;
+}

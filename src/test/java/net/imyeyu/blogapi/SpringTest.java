192393c74733491bb8a8d8b8f868647f2e2caa8b
Nagiasu <imyeyu@qq.com>
2021-08-02 09:29:47 +0800

    测试类重命名

--- src/test/java/net/imyeyu/blogapi/SpringTest.java
+++ src/test/java/net/imyeyu/blogapi/SpringTest.java
@@ -1,7 +1,8 @@
 package net.imyeyu.blogapi;
 
 import net.imyeyu.blogapi.bean.ServiceException;
-import net.imyeyu.blogapi.service.ArticleService;
+import net.imyeyu.blogapi.service.FriendChainService;
+import net.imyeyu.blogapi.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +11,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class ArticleTest {
+public class SpringTest {
 
 	@Autowired
-	private ArticleService service;
+	private UserService service;
 
 	@Test
 	public void test() throws ServiceException {
-//		System.out.println(new Gson().toJson(service.find(1L)));
+		System.out.println(service.find(3L));
 	}
 }

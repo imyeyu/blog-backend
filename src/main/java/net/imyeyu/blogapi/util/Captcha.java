76943caf6d68f2822ef03ea08910b915dd8865ad
Takagi <imyeyu@qq.com>
2021-09-23 13:09:58 +0800

    验证码移除易混淆字符 O

--- src/main/java/net/imyeyu/blogapi/util/Captcha.java
+++ src/main/java/net/imyeyu/blogapi/util/Captcha.java
@@ -38,7 +38,7 @@ public class Captcha {
 		code = new StringBuilder();
 		char[] c = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
 				'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'F',
-				'G', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+				'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 		// 移除 O,o,I,l
 		g.setFont(new Font("Fixedsys", Font.BOLD, 20));
 		for (int i = 0; i < 4; i++) {
